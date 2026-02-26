f2f2f373eaaac23df2da31fcf718966bc300cea7
nmizin <nmizin@blinkparametric.com>
2026-02-26 13:38:18 +0000

    Finished dailyTemperatures Python

---
+++ exercises/leet_code/daily_temperatures.py
@@ -0,0 +1,38 @@
+"""
+Q2. Daily Temperatures
+
+Given an array of integers temperatures represents the daily temperatures,
+return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature.
+If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
+
+
+Example 1:
+
+Input: temperatures = [73,74,75,71,69,72,76,73]
+Output: [1,1,4,2,1,1,0,0]
+Example 2:
+
+Input: temperatures = [30,40,50,60]
+Output: [1,1,1,0]
+Example 3:
+
+Input: temperatures = [30,60,90]
+Output: [1,1,0]
+"""
+
+
+def dailyTemperatures(temperatures: list[int]) -> list[int]:
+    stack = []
+    output = [0] * len(temperatures)
+    
+    for idx, temp in enumerate(temperatures):
+        while stack and temperatures[stack[-1]] < temp:
+            output[stack[-1]] = idx - stack[-1]
+            stack.pop()
+        stack.append(idx)
+    
+    return output
+    
+temperatures = [73,74,75,71,69,72,76,73]
+dailyTemperatures(temperatures)
